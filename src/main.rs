b6c1ea759ef3eec0763e908c4238369f8d2f2600
Bryce Colson <brycec17@vt.edu>
2021-09-10 00:19:30 -0700

    Test commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use gtk::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let app = gtk::Application::new(Some("com.odin-project.huginn"), Default::default());
+    app.connect_activate(build_ui);
+    app.run();
+}
+
+fn build_ui(app: &gtk::Application) {
+    let window = gtk::ApplicationWindow::new(app);
+    window.set_title("Huginn");
+    window.set_border_width(10);
+    window.set_position(gtk::WindowPosition::Center);
+
+    window.show_all();
 }
