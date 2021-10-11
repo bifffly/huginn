e52f2f1893bf5a6d57ed6296bba88b3ff01ff912
Bryce Colson <brycec17@vt.edu>
2021-10-11 12:11:05 -0700

    Updated renderer

--- src/components/Browser.tsx
+++ src/components/Browser.tsx
@@ -9,7 +9,6 @@ export default function Browser() {
 
   const navigate = (navUrl: string) => {
     setUrl(navUrl);
-    console.log(url);
   };
 
   const back = () => {
--- src/components/Renderer.tsx
+++ src/components/Renderer.tsx
@@ -5,32 +5,55 @@ export default function Renderer(props: {
   content: string
 }) {
   let lines = props.content.split('\n');
-  console.log(lines);
+
+  let content = [];
+  let items = [];
+  for (const line of lines) {
+    let args = line.split('\t');
+    let type = args.splice(0, 1)[0];
+
+    if (type === 't') {
+      document.title = "Huginn -- " + args[0];
+    }
+    if (type === 'h1') {
+      content.push(<h1>{args[0]}</h1>);
+    }
+    else if (type === 'h2') {
+      content .push(<h2>{args[0]}</h2>);
+    }
+    else if (type === 'h3') {
+      content.push(<h3>{args[0]}</h3>);
+    }
+    else if (type === 'p') {
+      content.push(<p>{args[0]}</p>);
+    }
+    else if (type === 'r') {
+      content.push(<a href=''>{args[0]}</a>);
+      content.push(<br/>);
+    }
+    else if (type === 'lbr') {
+      content.push(<br/>);
+    }
+    else if (type === 'lsep') {
+      content.push(<hr/>);
+    }
+    else if (type === 'unel') {
+      content.push(<ul>{items}</ul>);
+      items = [];
+    }
+    else if (type === 'unbl') {
+      content.push(<ol>{items}</ol>);
+      items = [];
+    }
+    else if (type === 'it') {
+       items.push(<li>{args[0]}</li>);
+    }
+  }
 
   return (
     <Container>
-      {lines.map(line => (
-        <OdinBlock line={line}/>
-      ))}
+      {content}
     </Container>
-  );
-}
-
-export function OdinBlock(props: {
-  line: string
-}) {
-  let args = props.line.split('\t');
-  let type = args.splice(0, 1)[0];
-
-  return (
-    <div>
-      {type === 'h1' ? <h1>{args[0]}</h1> : null}
-      {type === 'h2' ? <h2>{args[0]}</h2> : null}
-      {type === 'h3' ? <h3>{args[0]}</h3> : null}
-      {type === 'p' ? <p>{args[0]}</p> : null}
-      {type === 'lbr' ? <br/> : null}
-      {type === 'lsep' ? <hr/> : null}
-    </div>
   );
 }
 
